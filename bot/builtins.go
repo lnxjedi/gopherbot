102fd86b0587537c9e33dff72ea71721f999a54a
David Parsley <dlp7y@virginia.edu>
2016-04-24 10:24:47 -0400

    Update plugin text

--- bot/builtins.go
+++ bot/builtins.go
@@ -39,18 +39,20 @@ func help(bot Robot, channel, user, command string, args ...string) {
 	b := bot.robot
 	if command == "barehelp" { // user just typed 'help' - the robot should introduce itself
 		b.lock.RLock()
-		reply := "Hello, I'm "
+		reply := "Hi, I'm "
 		if len(b.fullName) > 0 {
 			reply += b.fullName + " - but you should just call me " + b.name + ".\n"
 		} else {
 			reply += b.name + ".\n"
 		}
-		reply += "I'm one of the staff robots available to your team, and can perform a variety of tasks. To find out what I can do, try:\n"
-		reply += b.name + ", help (keyword)\nor:\nhelp (keyword), " + b.name + "\n"
+		reply += "I'm one of the staff robots available to your team, and can respond in different ways to messages that match specific patterns. " +
+			"For instance, I will give you help on some of the things I can do when you send me a message that matches the word \"help\" followed by an optional keyword. " +
+			"You can address messages to me by using a direct message, or in a channel like this: \"" + b.name + ", help (keyword)\". For instance:\n`"
+		reply += b.name + ", help ping`\nor:\n`help ping, " + b.name + "`\nwould give you help on my ping command.\n"
 		if b.alias != 0 {
-			reply += "To save a little typing, you can prefix your message with my alias (" + string(b.alias) + "), like this:\n" + string(b.alias) + "help (keyword)\n"
+			reply += "To save a little typing, you can also direct a message to me by prefixing it with my alias (" + string(b.alias) + "), like this:\n`" + string(b.alias) + "help ping`\n"
 		}
-		reply += "The (keyword) is optional. If not supplied, you will get help for every command available to you in the current channel. If supplied, you will get help for every command with a matching keyword, along with the channels where it can be used. In all cases help will be sent as a direct message so the channels don't fill up with help text.\n"
+		reply += "When you see (something) in parentheses, that term or phrase is optional. If <something> is in angle brackets, it's required. With the help function, if you don't supply a keyword you will get help for every command available to you in the current channel. If you use a keyword, you will get help for every command with a matching keyword, along with the channels where it can be used. In all cases help will be sent as a direct message so the channels don't fill up with help text.\n"
 		reply += "Additionally, some messages (like a bare 'help') will trigger commands as well, and help may or may not be available for those.\n\nFinally, if there's anything else you'd like to see me do, please contact my administrator"
 		if len(b.adminContact) > 0 {
 			reply += ", " + b.adminContact + "."
