10caa32a4690135599c0ffb6d52862037696bb51
David Parsley <dlp7y@virginia.edu>
2016-05-02 18:09:29 -0400

    A little more variety in joke openings

--- goplugins/knock/knock.go
+++ goplugins/knock/knock.go
@@ -19,10 +19,16 @@ var configured bool
 
 var openings = []string{
 	"Ok, I know a good one!",
-	"Hrm... ok, this is one of my favorites!",
+	"Hrm... ok, this is one of my favorites...",
 	"I'll see if I can think of one...",
 	"Another robot told me this one, tell me if you think it's funny",
-	"I saw this lame joke on the Internet - but it's soooo much funnier when a robot tells it:",
+	"I found lame joke on the Internet - but it's kinda funny when a robot tells it",
+	"I'll ask Watson(tm) if he knows any good ones and get back to you in a jiffy...",
+	"Hang on while I Google that for you (just kidding ;-)",
+	"Sure - Siri told me this one, but I think it's kind of dumb",
+	"Ok, here's a funny one I found in Hillary's email...",
+	"Yeah! I LOVE telling jokes!",
+	"Alright - I'll see if I can make my voice sound funny",
 }
 
 var phooey = []string{
