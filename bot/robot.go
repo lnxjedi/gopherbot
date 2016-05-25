8eaed51f7aa18d9101d4281ee0c4be3667b5b704
David L. Parsley <parsley@linuxjedi.org>
2016-05-25 12:36:17 -0400

    Fix CheckOTP build issue

--- bot/robot.go
+++ bot/robot.go
@@ -46,20 +46,20 @@ func (r Robot) CheckOTP(code string) bool {
 	otpKey := "bot:OTP:" + r.User
 	var userOTP otp.OTPConfig
 	updated := false
-	_, exists, err := r.checkoutDatum(otpKey, &userOTP, true)
+	lock, exists, err := r.checkoutDatum(otpKey, &userOTP, true)
 	if err != nil {
 		return false
 	}
 	defer func() {
 		if updated {
-			err := bot.updateDatum(otpKey, lock, &userOTP)
+			err := r.updateDatum(otpKey, lock, &userOTP)
 			if err != nil {
-				bot.Log(Error, fmt.Errorf("Saving OTP config: %v", err))
+				r.Log(Error, fmt.Errorf("Saving OTP config: %v", err))
 			}
 		} else {
 			// Well-behaved plugins will always do a Checkin when the datum hasn't been updated,
 			// in case there's another thread waiting.
-			bot.checkin(otpKey, lock)
+			r.checkin(otpKey, lock)
 		}
 	}()
 	if !exists {
