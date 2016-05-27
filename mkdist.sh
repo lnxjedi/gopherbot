c3af1807cb1d32a91e001437f6999603ca02fe95
David L. Parsley <parsley@linuxjedi.org>
2016-05-27 09:11:52 -0400

    Make things more releasable

--- mkdist.sh
+++ mkdist.sh
@@ -8,20 +8,17 @@ usage(){
 	cat <<EOF
 Usage: mkdist.sh <destdir>
 
-Generate distributable .zip files for the supported platforms,
-currently linux and macos (darwin).
+Generate distributable .zip files for the current platform.
 EOF
 	exit 0
 }
 
 [ $# -eq 0 ] && usage
 
-for PLATFORM in darwin linux
-do
-	./build.sh $PLATFORM
-	OUTFILE=$1/gopherbot-$GOPHERBOT_VERSION-$PLATFORM.zip
-	rm -f $OUTFILE
+eval `go env`
+./build.sh
+OUTFILE=$1/gopherbot-$GOPHERBOT_VERSION-$GOOS.zip
+rm -f $OUTFILE
 
-	echo "Creating $OUTFILE"
-	zip -r $OUTFILE gopherbot LICENSE README.md conf/ plugins/ util/ gopherbot.template/ --exclude *.swp
-done
+echo "Creating $OUTFILE"
+zip -r $OUTFILE gopherbot LICENSE README.md conf/ plugins/ util/ example.gopherbot/ --exclude *.swp
