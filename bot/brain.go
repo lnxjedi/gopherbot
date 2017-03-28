eea3eddd700d51af2f1368abfe1006b822dfbd65
David L. Parsley <parsley@linuxjedi.org>
2017-03-28 15:57:00 -0400

    Start redesigned brain core (unfinished)

--- bot/brain.go
+++ bot/brain.go
@@ -9,141 +9,258 @@ import (
 	"time"
 )
 
-// Maximum amount of time that a plugin can hold a lock on a datum.
-// If the plugin tries to UpdateDatum() after the timeout expires, it'll
-// get an error. Even an external plugin should be able to manage
-// 200 ms.
-const lockTimeout = 200 * time.Millisecond
-
 // Map of registered brains
 var brains = make(map[string]func(Handler, *log.Logger) SimpleBrain)
 
-// Global lock protecting the map of datum locks, individual datam,
-// and datumLock.checkouts.
-var dataLock sync.Mutex
+type brainOpType int
 
-// lock struct that protects a checked out datum
-type datumLock struct {
-	checkouts  int // the number of threads checking out this datum
-	sync.Mutex     // the lock that protects the datum
+const (
+	checkOutBytes brainOpType = iota
+	checkInBytes
+	updateBytes
+	quit
+)
+
+type brainOp struct {
+	opType brainOpType
+	opData interface{}
 }
 
-// A global map of all synchronized items - we'll add the "glist"
-var data = make(map[string]*datumLock)
+type checkOutRequest struct {
+	key   string
+	rw    bool
+	reply chan checkOutReply
+}
 
-// The lock is good as long a the token exists; token gets deleted
-// on expiration.
-var lockTokens = make(map[string]bool)
-var ltLock sync.Mutex
+type checkInRequest struct {
+	key   string
+	token string
+}
+
+type updateRequest struct {
+	key   string
+	token string
+	datum *[]byte
+	reply chan RetVal
+}
+
+type checkOutReply struct {
+	token  string
+	bytes  *[]byte
+	exists bool
+	retval RetVal
+}
+
+type memState int
+
+const (
+	newMemory memState = iota
+	seen
+	available
+)
+
+type memstatus struct {
+	state   memState
+	token   string // whoever has this token owns the lock for this memory
+	waiters []checkOutRequest
+}
+
+var brainChanEvents = make(chan brainOp)
+
+// how often does the robot cycle through memories and update state?
+// a value of time.Second means a lock will last between 1 and 2 seconds
+const memCycle = time.Second
+
+func replyToWaiter(m *memstatus) {
+	cr := m.waiters[0]
+	m.waiters = m.waiters[1:]
+	lt, d, e, r := getDatum(cr.key, true)
+	m.state = newMemory
+	m.token = lt
+	cr.reply <- checkOutReply{lt, d, e, r}
+}
+
+func getDatum(dkey string, rw bool) (token string, databytes *[]byte, exists bool, ret RetVal) {
+	if !keyRe.MatchString(dkey) {
+		err := fmt.Errorf("Invalid key supplied to checkout: %s", dkey)
+		Log(Error, err)
+		return "", nil, false, InvalidDatumKey
+	}
+	b.lock.RLock()
+	brain := b.brain
+	b.lock.RUnlock()
+	if brain == nil {
+		Log(Error, "Brain function called with no brain configured")
+		return "", nil, false, BrainFailed
+	}
+	if rw { // checked out read/write, generate a lock token
+		ltb := make([]byte, 8)
+		random.Read(ltb)
+		token = fmt.Sprintf("%x", ltb)
+	} else {
+		token = ""
+	}
+	var err error
+	var db []byte
+	db, exists, err = b.brain.Retrieve(dkey)
+	if err != nil {
+		return "", nil, false, BrainFailed
+	}
+	return token, &db, exists, Ok
+}
+
+func storeDatum(key string, datum *[]byte) (ret RetVal) {
+	b.lock.RLock()
+	brain := b.brain
+	b.lock.RUnlock()
+	if brain == nil {
+		Log(Error, "Brain function called with no brain configured")
+		return BrainFailed
+	}
+	err := b.brain.Store(key, *datum)
+	if err != nil {
+		Log(Error, fmt.Sprintf("Storing datum %s: %v", key, err))
+		ret = BrainFailed
+	}
+	return Ok
+}
+
+var brainRunning bool
+var brLock sync.RWMutex
+
+// runBrain is the select loop that serializes access to brain
+// functions and insures consistency.
+func runBrain() {
+	brainRunning = true
+	// map key to status
+	memories := make(map[string]*memstatus)
+	processMemories := time.Tick(memCycle)
+loop:
+	for {
+		select {
+		case evt := <-brainChanEvents:
+			switch evt.opType {
+			case checkOutBytes:
+				cr := evt.opData.(checkOutRequest)
+				memStat, exists := memories[cr.key]
+				if !exists {
+					lt, d, e, r := getDatum(cr.key, cr.rw)
+					if r != Ok {
+						cr.reply <- checkOutReply{lt, d, e, r}
+						break
+					}
+					if cr.rw {
+						m := &memstatus{
+							newMemory,
+							lt,
+							make([]checkOutRequest, 0, 2),
+						}
+						memories[cr.key] = m
+					}
+					cr.reply <- checkOutReply{lt, d, e, r}
+					break
+				}
+				if !cr.rw {
+					lt, d, e, r := getDatum(cr.key, cr.rw)
+					cr.reply <- checkOutReply{lt, d, e, r}
+					break
+				} // read-write request below
+				// if state is available, there are no waiters
+				if memStat.state == available {
+					lt, d, e, r := getDatum(cr.key, cr.rw)
+					memStat.state = newMemory
+					memStat.token = lt // this memory has a new owner now
+					memories[cr.key] = memStat
+					cr.reply <- checkOutReply{lt, d, e, r}
+				} else {
+					memStat.waiters = append(memStat.waiters, cr)
+					memories[cr.key] = memStat
+				}
+			case checkInBytes:
+				ci := evt.opData.(checkInRequest)
+				m := memories[ci.key]
+				// memory expired and somebody else owns it
+				if ci.token != m.token {
+					break
+				}
+				if len(m.waiters) > 0 {
+					replyToWaiter(m)
+					break
+				}
+				delete(memories, ci.key)
+			case updateBytes:
+				ur := evt.opData.(updateRequest)
+				m := memories[ur.key]
+				if ur.token != m.token {
+					ur.reply <- DatumLockExpired
+					break
+				}
+				ur.reply <- storeDatum(ur.key, ur.datum)
+				if len(m.waiters) > 0 {
+					replyToWaiter(m)
+					break
+				}
+				delete(memories, ur.key)
+			case quit:
+				break loop
+			}
+		case <-processMemories:
+			for _, m := range memories {
+				switch m.state {
+				case newMemory:
+					m.state = seen
+				case seen:
+					if len(m.waiters) > 0 {
+						replyToWaiter(m)
+						break
+					}
+					m.state = available
+				}
+			}
+		}
+	}
+	brLock.Lock()
+	brainRunning = false
+	brLock.Unlock()
+}
 
 const keyRegex = `[\w:]+` // keys can ony be word chars + separator (:)
 var keyRe = regexp.MustCompile(keyRegex)
 
-// checkout returns the []byte from the brain, with a lock that expires
-// after lockTimeout. It returns a lock token, a pointer to the raw []byte
-// data, true if the key exists, and a RetVal.
+// checkout returns the []byte from the brain, with a lock token granting
+// ownership for a limited time
 func checkout(d string, rw bool) (string, *[]byte, bool, RetVal) {
 	if !keyRe.MatchString(d) {
 		err := fmt.Errorf("Invalid key supplied to checkout: %s", d)
 		Log(Error, err)
 		return "", nil, false, InvalidDatumKey
 	}
-	if b.brain == nil {
-		Log(Error, "Brain function called with no brain configured")
-		return "", nil, false, BrainFailed
-	}
-	var dl *datumLock
-	dataLock.Lock() // wait for access to the global list
-	dl, ok := data[d]
-	if ok {
-		dl.checkouts++
-	} else {
-		dl = &datumLock{checkouts: 1}
-		data[d] = dl
-	}
-	dataLock.Unlock()
-	var lt string
-	if rw { // checked out read/write, generate a lock token
-		ltb := make([]byte, 8)
-		random.Read(ltb)
-		lt = fmt.Sprintf("%x", ltb)
-		ltLock.Lock()
-		lockTokens[lt] = true
-		ltLock.Unlock()
-	} else {
-		lt = ""
-	}
-	dl.Lock() // block until we get the lock for the datum
-	// Retrieve the datum from the brain before starting the timer
-	datum, exists, err := b.brain.Retrieve(d)
-	if err != nil {
-		dl.Unlock()
-		return "", nil, false, BrainFailed
-	}
-	if rw {
-		// once rw lock is acquired, spin off lock expiration thread
-		go func(lt string, dl *datumLock) {
-			time.Sleep(lockTimeout)
-			ltLock.Lock()
-			if _, ok := lockTokens[lt]; ok {
-				Log(Error, fmt.Sprintf("Lock token %s expired, releasing lock", lt))
-				delete(lockTokens, lt)
-				dataLock.Lock()
-				dl.Unlock()
-				dl.checkouts--
-				if dl.checkouts == 0 { // nobody was waiting
-					delete(data, d)
-				}
-				dataLock.Unlock()
-			}
-			ltLock.Unlock()
-		}(lt, dl)
-	} else { // read-only copy checked out
-		dataLock.Lock()
-		dl.Unlock()
-		dl.checkouts--
-		if dl.checkouts == 0 {
-			delete(data, d)
-		}
-		dataLock.Unlock()
-	}
-	return lt, &datum, exists, Ok
+	reply := make(chan checkOutReply)
+	cr := checkOutRequest{d, rw, reply}
+	brainChanEvents <- brainOp{checkOutBytes, cr}
+	r := <-reply
+	return r.token, r.bytes, r.exists, r.retval
 }
 
 // update sends updated []byte to the brain while holding the lock, or discards
 // the data and returns an error.
 func update(d, lt string, datum *[]byte) (ret RetVal) {
-	if b.brain == nil {
-		Log(Error, "Brain function called with no brain configured")
-		return BrainFailed
+	if lt == "" {
+		return Ok
 	}
-	dataLock.Lock() // acquire the global lock
-	dl, ok := data[d]
-	if !ok {
-		Log(Error, fmt.Sprintf("Update called on non-existent datum: %s", d))
-		return DatumNotFound
+	reply := make(chan RetVal)
+	ur := updateRequest{d, lt, datum, reply}
+	brainChanEvents <- brainOp{updateBytes, ur}
+	return <-reply
+}
+
+// checkinDatum is the internal version of CheckinDatum that uses the key as-is
+func checkinDatum(key, locktoken string) {
+	if locktoken == "" {
+		return
 	}
-	ltLock.Lock() // we hope to get this lock before the timeout thread does
-	if _, ok := lockTokens[lt]; ok {
-		err := b.brain.Store(d, *datum)
-		dl.Unlock() // unlock after we've updated, successful or not
-		if err != nil {
-			Log(Error, fmt.Sprintf("Storing datum %s: %v", d, err))
-			ret = BrainFailed
-		}
-		delete(lockTokens, lt)
-		dl.checkouts--
-		if dl.checkouts == 0 {
-			delete(data, d)
-		}
-		// when !ok, the lock token is expired and the dl is already unlocked
-	} else {
-		ret = DatumLockExpired
-	}
-	ltLock.Unlock()
-	// Up to now has been 'instant' (no blocking) since the global lock was acquired
-	dataLock.Unlock()
-	return
+	ci := checkInRequest{key, locktoken}
+	brainChanEvents <- brainOp{checkInBytes, ci}
 }
 
 // checkoutDatum is the robot internal version of CheckoutDatum that uses
@@ -160,28 +277,6 @@ func checkoutDatum(key string, datum interface{}, rw bool) (locktoken string, ex
 		}
 	}
 	return
-}
-
-// checkinDatum is the internal version of CheckinDatum that uses the key as-is
-func checkinDatum(key, locktoken string) {
-	if locktoken == "" {
-		return
-	}
-	ltLock.Lock()
-	if _, ok := lockTokens[locktoken]; ok { // see if the lock was even held
-		delete(lockTokens, locktoken)
-		dataLock.Lock()
-		dl, ok := data[key]
-		if ok {
-			dl.Unlock()
-			dl.checkouts--
-			if dl.checkouts == 0 {
-				delete(data, key)
-			}
-		}
-		dataLock.Unlock()
-	}
-	ltLock.Unlock()
 }
 
 // updateDatum is the internal version of UpdateDatum that uses the key as-is
