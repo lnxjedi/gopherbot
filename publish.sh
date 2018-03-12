c82262e9b17dd6f4dcfed5aeeb64d2fe991db606
David Parsley <parsley@linuxjedi.org>
2018-03-12 15:13:43 +0000

    (untested) update publish.sh

--- publish.sh
+++ publish.sh
@@ -2,12 +2,20 @@
 
 # publish.sh - copy the install archive to a distribution point
 
-. .publish
 VERSTRING=$(grep "var Version" bot/bot.go)
 VERSTRING=${VERSTRING#var }
 VERSTRING=${VERSTRING// /}
 eval $VERSTRING
 eval `go env`
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ $BRANCH = "master" ]
+then
+    TAG=$Version
+    RELEASE="$Version-stable"
+else
+    TAG="$BRANCH-snapshot"
+    RELEASE=$TAG
+    PRERELEASE="--prerelease"
+fi
 
-github-release circleci *zip --prerelease --github-repository lnxjedi/gopherbot --tag "circleci"
+github-release $RELEASE *zip --github-repository lnxjedi/gopherbot --tag $TAG --replace $PRERELEASE
