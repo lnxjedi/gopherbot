17e78082cd2a5ae9455d638b724338d3b61c80f8
David Parsley <parsley@linuxjedi.org>
2018-03-12 11:36:58 -0400

    Circleci builds (#16)
    * Moving to circleci
    
    * Custom version string
    
    * ci change
    
    * Start new publish.sh
    
    * (untested) update publish.sh
    
    * First working version
    
    * Test circleci config

--- publish.sh
+++ publish.sh
@@ -2,13 +2,22 @@
 
 # publish.sh - copy the install archive to a distribution point
 
-. .publish
 VERSTRING=$(grep "var Version" bot/bot.go)
 VERSTRING=${VERSTRING#var }
 VERSTRING=${VERSTRING// /}
 eval $VERSTRING
 eval `go env`
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+COMMIT=$(git rev-parse HEAD)
+if [ $BRANCH = "master" ]
+then
+    TAG=$Version
+    RELEASE="$Version-stable"
+else
+    TAG="$BRANCH-snapshot"
+    RELEASE=$TAG
+    PRERELEASE="--prerelease"
+    REPLACE="--replace"
+fi
 
-SRCFILE=gopherbot-$Version-$GOOS-$GOARCH.zip
-echo "Publishing $SRCFILE to $PREFIX/gopherbot/$Version/gopherbot.zip"
-aws s3 cp $SRCFILE $PREFIX/gopherbot/$Version/gopherbot.zip
+github-release $RELEASE *zip --github-repository lnxjedi/gopherbot --tag $TAG --commit $COMMIT $PRERELEASE $REPLACE
