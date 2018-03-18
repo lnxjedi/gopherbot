4383e14565377d89243d4101a0d98a41d25a0722
David Parsley <parsley@linuxjedi.org>
2018-03-18 16:54:51 +0000

    Bump snapshot version to 1.1.0

--- bot/bot.go
+++ bot/bot.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version is the current version of Gopherbot
-var Version = "v1.0.1-snapshot"
+var Version = "v1.1.0-snapshot"
 
 // mkdist.* creates a temporary commit.go that sets commit to the current
 // git commit in an init() function
