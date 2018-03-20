f743a89088338c59cf57face1141eb5923fbe8d8
David Parsley <parsley@linuxjedi.org>
2018-03-20 21:58:56 +0000

    Add extensions, weather plugin

--- plugins/weather.rb
+++ plugins/weather.rb
@@ -5,31 +5,26 @@ require 'json'
 # To install:
 # 1) Copy this file to plugins/weather.rb
 # 2) Enable in gopherbot.yaml like so:
-
 #ExternalPlugins:
 #- Name: weather
 #  Path: plugins/weather.rb
-
 # 3) Put your configuration in conf/plugins/weather.yaml:
-
 #Config:
 #  APIKey: <your openweathermap key>
 #  TemperatureUnits: imperial # or 'metric'
-#  Country: 'us' # or other ISO 3166 country code
+#  DefaultCountry: 'us' # or other ISO 3166 country code
 
 # load the Gopherbot ruby library and instantiate the bot
 require ENV["GOPHER_INSTALLDIR"] + '/lib/gopherbot_v1'
 bot = Robot.new()
 
-# 
 defaultConfig = <<'DEFCONFIG'
----
 Help:
 - Keywords: [ "weather" ]
-  Helptext: [ "(bot), weather in <city or zip code> - fetch the weather from OpenWeatherMap" ]
+  Helptext: [ "(bot), weather in <city(,country) or zip code> - fetch the weather from OpenWeatherMap" ]
 CommandMatchers:
 - Command: weather
-  Regex: '(?i:weather (?:in|for) ([\w ]+))'
+  Regex: '(?i:weather (?:in|for) (.+))'
 DEFCONFIG
 
 command = ARGV.shift()
@@ -40,9 +35,15 @@ when "configure"
 	exit
 when "weather"
     c = bot.GetPluginConfig()
-    uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{location},#{c["Country"]}&units=#{c["TemperatureUnits"]}&APPID=#{c["APIKey"]}"
+    location = ARGV.shift()
+    location += ",#{c["DefaultCountry"]}" unless location.include?(',')
+    uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{location}&units=#{c["TemperatureUnits"]}&APPID=#{c["APIKey"]}")
     d = JSON::parse(Net::HTTP.get(uri))
-    w = d["weather"][0]
-    t = d["main"]
-    bot.Say("The weather in #{d["name"]} is currently \"#{w["description"]}\" and #{t["temp"]} degrees, with a forecast low of #{t["temp_min"]} and high of #{t["temp_max"]}")
+    if d["message"]
+        bot.Say("Sorry: \"#{d["message"]}\", maybe try the zip code?")
+    else
+        w = d["weather"][0]
+        t = d["main"]
+        bot.Say("The weather in #{d["name"]} is currently \"#{w["description"]}\" and #{t["temp"]} degrees, with a forecast low of #{t["temp_min"]} and high of #{t["temp_max"]}")
+    end
 end
