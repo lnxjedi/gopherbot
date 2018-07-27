2a175e817fc5ae4e14e82741312c62b7ca1c1718
David Parsley <parsley@linuxjedi.org>
2018-07-27 12:48:15 +0000

    Add comment on args

--- tasks/notify.sh
+++ tasks/notify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 # tasks/notify.sh - send notification to a user, generally used as a FailTask
+# Requires two arguments: notify user and message
 
 source $GOPHER_INSTALLDIR/lib/gopherbot_v1.sh
 
