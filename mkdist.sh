57f9932c5c9bfec6941555b73b30985f117e4d76
David Parsley <parsley@linuxjedi.org>
2018-10-27 00:25:17 +0000

    Try again for static build

--- mkdist.sh
+++ mkdist.sh
@@ -41,7 +41,7 @@ do
 		zip -r $OUTFILE gopherbot.exe LICENSE README.md brain/ conf/ doc/ cfg/ lib/ licenses/ misc/ plugins/ jobs/ tasks/ scripts/ --exclude *.swp --exclude conf/*.yaml --exclude conf/*/*.yaml
 	elif [ "$BUILDOS" = "linux" ]
 	then
-		CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags 'netgo osusergo static_build' -o gopherbot
+		CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags 'netgo osusergo static_build' -o gopherbot
 		echo "Creating $OUTFILE"
 		zip -r $OUTFILE gopherbot LICENSE README.md brain/ conf/ doc/ cfg/ lib/ licenses/ misc/ plugins/ jobs/ tasks/ scripts/ --exclude *.swp --exclude conf/*.yaml --exclude conf/*/*.yaml
 	else
