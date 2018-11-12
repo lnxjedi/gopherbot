ff33263b2f7b12d01b924d77bfe1425cfd23a803
David Parsley <dlp7y@virginia.edu>
2018-11-12 13:56:30 -0500

    Remove unneeded hacky script

--- .gopherci/docker-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# docker-update.sh - run from root of repository to update the Dockerfiles
-# for image rebuilds.
-
-COMMIT=$(git rev-parse --short HEAD)
-
-sed -i "s/ARG token=.*/ARG token=$COMMIT/" resources/docker/amazonlinux/Dockerfile
-sed -i "s/ARG token=.*/ARG token=$COMMIT/" resources/docker/ubuntu/Dockerfile
