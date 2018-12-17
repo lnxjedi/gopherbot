0ed290c695e5184347fbf9ea467c22690556c932
David Parsley <dlp7y@virginia.edu>
2018-12-17 12:51:45 -0500

    new runpipeline task

--- tasks/runpipeline.sh
+++ tasks/runpipeline.sh
@@ -7,11 +7,11 @@ source $GOPHER_INSTALLDIR/lib/gopherbot_v1.sh
 
 for PTRY in pipeline.sh pipeline.py pipeline.rb
 do
-    if [ -x "./gopherbot/$PTRY" ]
+    if [ -x ".gopherci/$PTRY" ]
     then
-        AddTask exec ".gopherci/$PTRY"
+        AddTask exec "./.gopherci/$PTRY"
         exit 0
     fi
 done
 
-Log "Warn" "Repository pipeline not found in job $GOPHER_JOB_NAME (repo: ${GOPHERCI_REPO:-not set}), ignoring"
+Log "Warn" "Repository pipeline not found in job $GOPHER_JOB_NAME (wd: $(pwd), repo: ${GOPHERCI_REPO:-not set}), ignoring"
