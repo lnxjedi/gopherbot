15c10d9d937ea69c7b14c50844829883786ec51b
David Parsley <dlp7y@virginia.edu>
2018-12-21 14:34:52 -0500

    Squelch output of curl

--- .gopherci/dockercloud.sh
+++ .gopherci/dockercloud.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 # Trigger the automatic build 
-curl -X POST https://cloud.docker.com/$DOCKER_TRIGGER
+curl -X POST https://cloud.docker.com/$DOCKER_TRIGGER > /dev/null
+
+echo "Docker Cloud build triggered"
