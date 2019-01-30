5245a3cd56de53f1e3dbda8dbeb3269c9b9c3a91
David Parsley <dlp7y@virginia.edu>
2019-01-30 09:15:18 -0500

    Don't munge case in terminal connector when not in test mode

--- connectors/terminal/connector.go
+++ connectors/terminal/connector.go
@@ -148,31 +148,3 @@ loop:
 		}
 	}
 }
-
-func (tc *termConnector) sendMessage(ch, msg string, f bot.MessageFormat) (ret bot.RetVal) {
-	found := false
-	tc.RLock()
-	if strings.HasPrefix(ch, "(dm:") {
-		found = true
-	} else {
-		for _, channel := range tc.channels {
-			if channel == ch {
-				found = true
-				break
-			}
-		}
-	}
-	tc.RUnlock()
-	if !found {
-		tc.Log(bot.Error, "Channel not found:", ch)
-		return bot.ChannelNotFound
-	}
-	switch f {
-	case bot.Fixed:
-		msg = strings.ToUpper(msg)
-	case bot.Variable:
-		msg = strings.ToLower(msg)
-	}
-	tc.reader.Write([]byte(fmt.Sprintf("%s: %s\n", ch, msg)))
-	return bot.Ok
-}
---
+++ connectors/terminal/sendmessage.go
@@ -0,0 +1,32 @@
+// +build !test
+
+package terminal
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/lnxjedi/gopherbot/bot"
+)
+
+func (tc *termConnector) sendMessage(ch, msg string, f bot.MessageFormat) (ret bot.RetVal) {
+	found := false
+	tc.RLock()
+	if strings.HasPrefix(ch, "(dm:") {
+		found = true
+	} else {
+		for _, channel := range tc.channels {
+			if channel == ch {
+				found = true
+				break
+			}
+		}
+	}
+	tc.RUnlock()
+	if !found {
+		tc.Log(bot.Error, "Channel not found:", ch)
+		return bot.ChannelNotFound
+	}
+	tc.reader.Write([]byte(fmt.Sprintf("%s: %s\n", ch, msg)))
+	return bot.Ok
+}
