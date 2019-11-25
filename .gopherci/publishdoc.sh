f6bbccd0178d24b50e904ca329fc10786f8cefff
David Parsley <parsley@linuxjedi.org>
2019-11-25 14:37:57 -0500

    Improve commit message

--- .gopherci/publishdoc.sh
+++ .gopherci/publishdoc.sh
@@ -12,6 +12,6 @@ then
 fi
 
 git add .
-git commit -m "Updates from commit $COMMIT"
+git commit -m "Updates from lnxjedi/gopherbot commit $COMMIT"
 git remote add update git@github.com:lnxjedi/gopherbot-doc.git
 git push -u update master
