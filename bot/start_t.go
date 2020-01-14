a0e033937e2bf717e8f7ac30090c14f6ba32927f
David Parsley <dlp7y@virginia.edu>
2020-01-14 11:37:42 -0500

    Fix race in test harness

--- bot/start_t.go
+++ bot/start_t.go
@@ -25,33 +25,40 @@ func init() {
 
 // StartTest will start a robot for testing, and return the exit / robot stopped channel
 func StartTest(v VersionInfo, cfgdir, logfile string, t *testing.T) (chan bool, robot.Connector) {
-	var testLogger *log.Logger
 	botVersion = v
 	configpath := filepath.Join(testInstallPath, cfgdir)
-	t.Logf("Initializing test bot with installpath: \"%s\" and configpath: \"%s\"", testInstallPath, configpath)
+	t.Logf("Initializing test bot with installpath: '%s', configpath: '%s' and logfile: %s", testInstallPath, configpath, logfile)
 
-	botStdOutLogger = log.New(os.Stdout, "", log.LstdFlags)
-	if len(logfile) == 0 {
-		testLogger = log.New(ioutil.Discard, "", 0)
+	if botLogger.l == nil {
+		var testLogger *log.Logger
+		botStdOutLogger = log.New(os.Stdout, "", log.LstdFlags)
+		if len(logfile) == 0 {
+			testLogger = log.New(ioutil.Discard, "", 0)
+		} else {
+			lf, err := os.Create(logfile)
+			if err != nil {
+				log.Fatalf("Error creating log file: (%T %v)", err, err)
+			}
+			testLogger = log.New(lf, "", log.LstdFlags)
+		}
+		botLogger.l = testLogger
 	} else {
 		lf, err := os.Create(logfile)
 		if err != nil {
 			log.Fatalf("Error creating log file: (%T %v)", err, err)
 		}
-		testLogger = log.New(lf, "", log.LstdFlags)
+		botLogger.setOutputFile(lf)
 	}
-
-	botLogger.l = testLogger
 	initBot(configpath, testInstallPath)
 
 	initializeConnector, ok := connectors[currentCfg.protocol]
 	if !ok {
-		testLogger.Fatalf("No connector registered with name: %s", currentCfg.protocol)
+		Log(robot.Fatal, "No connector registered with name: %s", currentCfg.protocol)
 	}
-	testLogger.Printf("Starting new test with cfgdir: %s\n", cfgdir)
+	Log(robot.Info, "Starting new test with cfgdir: %s\n", cfgdir)
 
 	// handler{} is just a placeholder struct for implementing the Handler interface
-	conn := initializeConnector(handle, testLogger)
+	conn := initializeConnector(handle, botLogger.l)
 
 	// NOTE: we use setConnector instead of passing the connector to run()
 	// because of the way Windows services were run. Maybe remove eventually?
