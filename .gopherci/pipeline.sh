aeaab9c8c64b26faff21e968f674562bdbfbb0c8
David Parsley <parsley@linuxjedi.org>
2020-04-20 13:14:21 -0400

    Update doc-only pipeline

--- .gopherci/pipeline.sh
+++ .gopherci/pipeline.sh
@@ -20,12 +20,17 @@ FailTask email-log parsley@linuxjedi.org
 # Docs only
 if [ "$GOPHER_REPOSITORY" == "github.com/lnxjedi/gopherbot" -a "$GOPHERCI_BRANCH" == "docs" ]
 then
-    AddTask exec ./.gopherci/mkdocs.sh
     # Initialize ssh for updating docs repo
     AddTask ssh-init
 
     # Make sure github is in known_hosts
     AddTask ssh-scan github.com
+
+    # Clone existing gh-pages
+    AddTask git-clone https://github.com/lnxjedi/gopherbot.git gh-pages gopherbot-doc
+
+    # Build new
+    AddTask exec ./.gopherci/mkdocs.sh
 
     # Publish doc updates (if any)
     AddTask exec ./.gopherci/publishdoc.sh
