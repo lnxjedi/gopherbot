943d24369af0c776b4c301fd1b1aa729f46ca1a0
David Parsley <parsley@linuxjedi.org>
2021-03-04 09:37:49 -0500

    Set version to 2.0

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/lnxjedi/gopherbot/bot"
 
 // Version - major version of gopherbot
-var Version = "v2"
+var Version = "v2.0"
 
 // Commit supplied during linking
 var Commit = "(not set)"
