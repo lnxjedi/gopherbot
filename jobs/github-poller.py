c30a53d340b6109e5fa7e7abc2233c94b1a3e7ef
David Parsley <parsley@linuxjedi.org>
2021-03-17 15:05:33 +0000

    Refactor for shorter memlock, error checking, fixes #217

--- jobs/github-poller.py
+++ jobs/github-poller.py
@@ -12,24 +12,84 @@ sys.path.append("%s/lib" % os.getenv("GOPHER_INSTALLDIR"))
 from gopherbot_v2 import Robot
 
 bot = Robot()
+
+def error_finish():
+    log_ref = os.getenv("GOPHER_LOG_REF")
+    bot.Say("There were errors polling github repositories, log: %s" % log_ref)
+    exit(0)
+
 token = os.getenv("GITHUB_TOKEN", '...')
 if token == '...':
     bot.Log("Error", "GITHUB_TOKEN not found in environment")
-    exit(0)
+    error_finish()
 
 repodata = bot.GetRepoData()
 
 if not isinstance(repodata, dict):
     bot.Log("Warn", "github-poller triggered with invalid 'repositories.yaml', not a python 'dict'")
-    exit(0)
-
-# Pop off the executable path
-sys.argv.pop(0)
+    error_finish()
 
 prefix = "https://api.github.com/repos"
-first_run = False
+fetch_error = False
+remotes = {}
+want_builds = {}
+
+def fetch_refs(url):
+    global fetch_error
+    req = urllib.request.Request(url=url)
+    req.add_header('Authorization', f'token {token}')
+    try:
+        res = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        fetch_error = True
+        bot.Log("Error", "Fetching '%s': (%d) %s" % (url, e.code, e.read()))
+        return {}, False
+    body = res.read()
+    return json.loads(body.decode("utf-8")), True
+
+def read_repo(reponame, repoconf):
+    _, org, name = reponame.split("/")
+    fullname = "%s/%s" % (org, name)
+    print("Checking repo %s" % reponame)
+    refs = {}
+
+    tagurl = "%s/%s/tags" % (prefix, fullname)
+    tags, ok = fetch_refs(tagurl)
+    if ok:
+        for t in list(tags):
+            name = t["name"]
+            commit = t["commit"]["sha"]
+            print("Found %s : %s / %s" % (reponame, name, commit))
+            refs[name] = commit
+    else:
+        return {}
+
+    branchurl = "%s/%s/branches" % (prefix, fullname)
+    branches, ok = fetch_refs(branchurl)
+    if ok:
+        for b in list(branches):
+            name = b["name"]
+            commit = b["commit"]["sha"]
+            print("Found %s : %s / %s" % (reponame, name, commit))
+            refs[name] = commit
+    else:
+        return {}
+
+    return refs
+
+# Read refs for all github repos and store in remotes dict
+for reponame in repodata.keys():
+    host, org, name = reponame.split("/")
+    if host == "github.com":
+        repoconf = repodata[reponame]
+        repotype = repoconf["Type"]
+        if len(repotype) != 0 and repotype != "none":
+            refs = read_repo(reponame, repoconf)
+            if len(refs) > 0:
+                remotes[reponame] = refs
 
 # Retrive repo status memory
+first_run = False
 memory = bot.CheckoutDatum("repostats", True)
 if not memory.exists:
     first_run = True
@@ -38,36 +98,12 @@ repostats = memory.datum
 
 want_builds = {}
 
-def fetch_refs(url):
-    req = urllib.request.Request(url=url)
-    req.add_header('Authorization', f'token {token}')
-    res = urllib.request.urlopen(req)
-    body = res.read()
-    return json.loads(body.decode("utf-8"))
-
-def check_repo(reponame, repoconf):
-    _, org, name = reponame.split("/")
-    fullname = "%s/%s" % (org, name)
-    if not fullname in repostats:
-        repostats[fullname] = {}
-    repostat = repostats[fullname]
-    print("Checking repo %s" % fullname)
-    refs = {}
-
-    tagurl = "%s/%s/tags" % (prefix, fullname)
-    tags = fetch_refs(tagurl)
-    for t in list(tags):
-        name = t["name"]
-        commit = t["commit"]["sha"]
-        refs[name] = commit
-
-    branchurl = "%s/%s/branches" % (prefix, fullname)
-    branches = fetch_refs(branchurl)
-    for b in list(branches):
-        name = b["name"]
-        commit = b["commit"]["sha"]
-        refs[name] = commit
-
+for reponame in remotes.keys():
+    repostat = {}
+    if not reponame in repostats:
+        repostats[reponame] = {}
+    repostat = repostats[reponame]
+    refs = remotes[reponame]
     for name in list(refs):
         commit = refs[name]
         last = ""
@@ -77,34 +113,28 @@ def check_repo(reponame, repoconf):
         build = False
         if commit != last:
             build = True
-        print("Found %s / %s: last built: %s, current: %s, build: %s" % (fullname, name, last, commit, build))
+        print("Evaluating %s / %s: last built: %s, current: %s, build: %s" % (reponame, name, last, commit, build))
         if build:
             repotype = repoconf["Type"]
             if first_run:
-                bot.Log("Debug", "Skipping primary build for %s (branch %s) to the pipeline, type '%s' (first run)" % (reponame, name, repotype))
+                print("Skipping primary build for %s (branch %s) to the pipeline, type '%s' (first run)" % (reponame, name, repotype))
             else:
-                bot.Log("Debug", "Adding primary build for %s (branch %s) to the pipeline, type '%s'" % (reponame, name, repotype))
+                print("Adding primary build for %s (branch %s) to the pipeline, type '%s'" % (reponame, name, repotype))
                 want_builds[reponame] = name
-    if len(refs) > 0:
-        for name in list(repostat):
-            if not name in refs:
-                bot.Log("Debug", "Pruning %s from %s, no longer present" % (name, fullname))
-                repostat.pop(name)
-
-for reponame in repodata.keys():
-    host, org, name = reponame.split("/")
-    if host == "github.com":
-        repoconf = repodata[reponame]
-        repotype = repoconf["Type"]
-        if len(repotype) != 0 and repotype != "none":
-            check_repo(reponame, repoconf)
+    for name in list(repostat):
+        if not name in refs:
+            print("Pruning %s from %s, no longer present" % (name, reponame))
+            repostat.pop(name)
 
 memory.datum = repostats
 ret = bot.UpdateDatum(memory)
 if ret != Robot.Ok:
     bot.Log("Error", "Unable to save long-term memory in github-poller: %s" % ret)
-    exit(1)
+    error_finish()
 
 for reponame in list(want_builds):
     name = want_builds[reponame]
     bot.SpawnJob("gopherci", [ "build", reponame, name ])
+
+if fetch_error:
+    error_finish()
