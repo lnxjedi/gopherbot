e8b11a239f00aa3c94dbdd28d7da11fae6afe9fa
David L. Parsley <dlp7y@virginia.edu>
2021-03-18 08:37:05 -0400

    Convenience script

---
+++ setuid-nobody.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# setuid-nobody.sh - trivial convenience script for common permissions on
+# gopherbbot binary; prevents silly typos.
+
+if [ $(id -u) -ne 0 ]
+then
+	echo "Must be run as root"
+	exit 1
+fi
+
+INSTALLDIR=$(dirname $0)
+cd $INSTALLDIR
+chown nobody gopherbot
+chmod u+s gopherbot
+echo "Done."
