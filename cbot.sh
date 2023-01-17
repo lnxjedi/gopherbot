c0fcab4b5dcb035d5976e9a863c843c2a1f2f6ae
David Parsley <david@welldhealth.com>
2023-01-17 11:32:12 -0500

    Set GOPHER_IDE

--- cbot.sh
+++ cbot.sh
@@ -504,6 +504,7 @@ EOF
             -p 127.0.0.1:7777:7777 \
             -p 127.0.0.1:8888:8888 \
             --env-file $GOPHER_PROFILE \
+            -e GOPHER_IDE="$CONTAINERNAME" \
             -l type=gopherbot/robot \
             -l environment=robot/development \
             -l access=$(get_access) \
