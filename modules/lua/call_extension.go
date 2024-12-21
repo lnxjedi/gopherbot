f8f54504492f9408ca7b5a2143f2d02214e044a2
David Parsley <parsley@linuxjedi.org>
2024-12-21 23:02:41 +0000

    More constants, complete send_messages methods

--- modules/lua/call_extension.go
+++ modules/lua/call_extension.go
@@ -15,120 +15,79 @@ type luaRobot struct {
 
 // CallExtension loads and executes a Lua script at taskPath,
 // passing in the robot.Robot, privileged mode, and the string arguments.
-// It returns a robot.TaskRetVal and an error (if something goes wrong
-// in the Lua execution).
 func CallExtension(taskPath, taskName string, r robot.Robot, privileged bool, args []string) (robot.TaskRetVal, error) {
 	// 1. Create a new Lua state.
 	L := glua.NewState()
 	defer L.Close()
 
-	// 2. Optionally load only certain libraries based on privileged or unprivileged.
-	//    For now, load all standard libraries for demonstration.
+	// 2. Optionally load only certain libraries if privileged. For now, load all.
 	L.OpenLibs()
 
-	// 3. Register the Robot userdata type and set its methods.
+	// 3. Register the "robot" type and base methods.
 	registerRobotType(L)
 
-	// 4. Create a userdata object and assign the actual robot to it.
+	// 4. Now register additional method sets (message methods, logging, etc.)
+	//    Just call them here (or do it inside registerRobotType, whichever).
+	RegisterMessageMethods(L)
+	// Example: RegisterLogMethods(L)
+
+	// 5. Create a userdata object and assign the actual robot to it.
 	robotUD := L.NewUserData()
 	robotUD.Value = &luaRobot{r: r}
 	L.SetMetatable(robotUD, L.GetTypeMetatable("robot"))
-	// Make it globally accessible as "robot".
 	L.SetGlobal("robot", robotUD)
 
-	// 5. Register constants for robot.RetVal (method returns) and robot.TaskRetVal (script returns).
+	// 6. Register constants (RetVal, TaskRetVal, LogLevel, etc.).
 	registerConstants(L)
 
-	// 6. Set the global "args" table so Lua can access the arguments.
+	// 7. Provide the args table
 	argsTable := L.CreateTable(len(args), 0)
 	for i, a := range args {
 		argsTable.RawSetInt(i+1, glua.LString(a))
 	}
 	L.SetGlobal("args", argsTable)
 
-	// 7. Compile and run the Lua file.
+	// 8. Compile and run the Lua file.
 	if err := L.DoFile(taskPath); err != nil {
-		// A compile or runtime error in Lua => MechanismFail
 		r.Log(robot.Error, fmt.Sprintf("Lua error in script '%s': %v", taskName, err))
 		return robot.MechanismFail, err
 	}
 
-	// 8. After successful script execution, check what the script returned.
-	//    By convention, we expect exactly one return value from the script.
-	//    If the script didn’t explicitly return anything, that’s fine — we’ll default to Normal.
+	// 9. Gather the final return value (defaults to robot.Normal).
 	retVal := L.Get(-1) // top of the stack
-	L.Pop(1)            // pop it off
+	L.Pop(1)
 
-	// Convert that Lua value to an int => robot.TaskRetVal
 	var taskReturn robot.TaskRetVal = robot.Normal
 	if ln, ok := retVal.(glua.LNumber); ok {
-		taskReturn = robot.TaskRetVal(ln) // e.g. 0 => Normal, 1 => Fail, etc.
+		taskReturn = robot.TaskRetVal(ln)
 	}
-
-	// 9. Return the final task return value.
 	return taskReturn, nil
 }
 
-// registerRobotType sets up a metatable for "robot" userdata and binds Go
-// methods (like :Say) to be callable from Lua.
+// registerRobotType creates a "robot" metatable with base methods.
 func registerRobotType(L *glua.LState) {
-	// Create a metatable for type "robot".
 	mt := L.NewTypeMetatable("robot")
-	// Set __index to a table of methods.
+	// Start with a new table for __index, and register base methods
 	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), robotMethods))
 }
 
-// robotMethods binds each robot.* Lua method name to the corresponding Go function.
+// robotMethods are the base set of methods we consider “core” to Robot.
+// Additional sets (like SendMessage, Log, etc.) get added separately.
 var robotMethods = map[string]glua.LGFunction{
-	"Say": robotSay,
-	// In the future, add more: "Reply", "Log", etc.
+	// For example, if you have an "Elevate" or "CheckAdmin" method, put it here.
+	// "Say" or "Reply" can go here or in a separate file, depending on preference.
 }
 
-// robotSay implements the robot:Say("some message") call.
-func robotSay(L *glua.LState) int {
-	// 1st argument is the userdata (self), 2nd is the message string.
-	ud := L.CheckUserData(1)
-	msg := L.CheckString(2)
-
-	lr, ok := ud.Value.(*luaRobot)
-	if !ok {
-		// RaiseError throws a Lua runtime error.
-		L.RaiseError("invalid robot userdata")
-		return 0
+// getRobotMethodTable fetches the existing __index table for the "robot" type,
+// so we can add more methods without overwriting the entire table.
+func getRobotMethodTable(L *glua.LState) *glua.LTable {
+	mt := L.GetTypeMetatable("robot") // get metatable for "robot"
+	idx := L.GetField(mt, "__index")  // get the current __index
+	if idxTable, ok := idx.(*glua.LTable); ok {
+		return idxTable
 	}
-
-	// Call the actual Go robot’s Say method.
-	ret := lr.r.Say(msg)
-	// Push the RetVal (int) onto the Lua stack.
-	L.Push(glua.LNumber(ret))
-	// Return 1 value back to Lua (the ret code).
-	return 1
-}
-
-// registerConstants registers a minimal set of robot.RetVal and robot.TaskRetVal constants
-// into the global Lua environment.
-func registerConstants(L *glua.LState) {
-	// --------------------------------------------------------------------
-	// Robot method return codes (RetVal) => "retXYZ"
-	// e.g., retOk, retFailedMessageSend, etc.
-	// --------------------------------------------------------------------
-	L.SetGlobal("retOk", glua.LNumber(robot.Ok))
-	L.SetGlobal("retUserNotFound", glua.LNumber(robot.UserNotFound))
-	L.SetGlobal("retChannelNotFound", glua.LNumber(robot.ChannelNotFound))
-	L.SetGlobal("retFailedMessageSend", glua.LNumber(robot.FailedMessageSend))
-	// ... Add more as needed ...
-	// L.SetGlobal("retInterrupted", glua.LNumber(robot.Interrupted))
-	// L.SetGlobal("retReplyNotMatched", glua.LNumber(robot.ReplyNotMatched))
-	// etc.
-
-	// --------------------------------------------------------------------
-	// Task return codes (TaskRetVal) => "taskXYZ"
-	// e.g., taskNormal, taskFail, taskMechanismFail, etc.
-	// --------------------------------------------------------------------
-	L.SetGlobal("taskNormal", glua.LNumber(robot.Normal)) // 0
-	L.SetGlobal("taskFail", glua.LNumber(robot.Fail))     // 1
-	L.SetGlobal("taskMechanismFail", glua.LNumber(robot.MechanismFail))
-	L.SetGlobal("taskConfigurationError", glua.LNumber(robot.ConfigurationError))
-	L.SetGlobal("taskSuccess", glua.LNumber(robot.Success)) // 7
-	// ... Add more as needed ...
+	// If it wasn't a table, create a new one just in case.
+	newTable := L.NewTable()
+	L.SetField(mt, "__index", newTable)
+	return newTable
 }
---
+++ modules/lua/send_messages.go
@@ -0,0 +1,227 @@
+package lua
+
+import (
+	"github.com/lnxjedi/gopherbot/robot"
+	glua "github.com/yuin/gopher-lua"
+)
+
+// getRobotWithOptionalFormat is a helper function to:
+// 1) Extract the luaRobot from stack index 1
+// 2) If present, parse a message format argument
+// Returns:
+//
+//	(robot.Robot, error string) - if errStr != "" then raise Lua error
+func getRobotWithOptionalFormat(L *glua.LState, formatIndex int) (robot.Robot, string) {
+	ud := L.CheckUserData(1)
+	lr, ok := ud.Value.(*luaRobot)
+	if !ok {
+		return nil, "invalid robot userdata"
+	}
+
+	r := lr.r
+
+	// If the caller supplied an argument for format, parse it
+	if L.GetTop() >= formatIndex {
+		fmtArg := L.Get(formatIndex)
+		if num, isNum := fmtArg.(glua.LNumber); isNum {
+			format := robot.MessageFormat(int(num))
+			r = r.MessageFormat(format)
+		}
+	}
+	return r, ""
+}
+
+// robotSay(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:Say("Hello world", fmtRaw)
+func robotSay(L *glua.LState) int {
+	// 2nd arg is the message string
+	msg := L.CheckString(2)
+
+	// Setup Robot + optional format at stack index 3
+	r, errStr := getRobotWithOptionalFormat(L, 3)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.Say(msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotReply(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:Reply("Hello user", fmtVariable)
+func robotReply(L *glua.LState) int {
+	msg := L.CheckString(2)
+
+	r, errStr := getRobotWithOptionalFormat(L, 3)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.Reply(msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSayThread(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SayThread("Hello in a new thread", fmtRaw)
+func robotSayThread(L *glua.LState) int {
+	msg := L.CheckString(2)
+
+	r, errStr := getRobotWithOptionalFormat(L, 3)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SayThread(msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotReplyThread(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:ReplyThread("Replying in a new thread", fmtFixed)
+func robotReplyThread(L *glua.LState) int {
+	msg := L.CheckString(2)
+
+	r, errStr := getRobotWithOptionalFormat(L, 3)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.ReplyThread(msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSendChannelMessage(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SendChannelMessage("my-channel", "Hello channel", fmtFixed)
+func robotSendChannelMessage(L *glua.LState) int {
+	channel := L.CheckString(2)
+	msg := L.CheckString(3)
+
+	// Optional format is 4th arg
+	r, errStr := getRobotWithOptionalFormat(L, 4)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SendChannelMessage(channel, msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSendChannelThreadMessage(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SendChannelThreadMessage("my-channel", "thread-id", "Hello thread", fmtRaw)
+func robotSendChannelThreadMessage(L *glua.LState) int {
+	channel := L.CheckString(2)
+	thread := L.CheckString(3)
+	msg := L.CheckString(4)
+
+	// Optional format is 5th arg
+	r, errStr := getRobotWithOptionalFormat(L, 5)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SendChannelThreadMessage(channel, thread, msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSendUserMessage(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SendUserMessage("some.user", "Hello user", fmtRaw)
+func robotSendUserMessage(L *glua.LState) int {
+	user := L.CheckString(2)
+	msg := L.CheckString(3)
+
+	r, errStr := getRobotWithOptionalFormat(L, 4)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SendUserMessage(user, msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSendUserChannelMessage(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SendUserChannelMessage("some.user", "some-channel", "Hello in channel", fmtVariable)
+func robotSendUserChannelMessage(L *glua.LState) int {
+	user := L.CheckString(2)
+	channel := L.CheckString(3)
+	msg := L.CheckString(4)
+
+	r, errStr := getRobotWithOptionalFormat(L, 5)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SendUserChannelMessage(user, channel, msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// robotSendUserChannelThreadMessage(luaState) -> retVal
+// Usage in Lua:
+//
+//	local ret = robot:SendUserChannelThreadMessage("some.user", "some-channel", "some-thread", "Hello in thread", fmtFixed)
+func robotSendUserChannelThreadMessage(L *glua.LState) int {
+	user := L.CheckString(2)
+	channel := L.CheckString(3)
+	thread := L.CheckString(4)
+	msg := L.CheckString(5)
+
+	r, errStr := getRobotWithOptionalFormat(L, 6)
+	if errStr != "" {
+		L.RaiseError(errStr)
+		return 0
+	}
+
+	ret := r.SendUserChannelThreadMessage(user, channel, thread, msg)
+	L.Push(glua.LNumber(ret))
+	return 1
+}
+
+// RegisterMessageMethods can be called from your "registerRobotType" logic
+// to add these methods into the robot's metatable.
+func RegisterMessageMethods(L *glua.LState) {
+	messageMethods := map[string]glua.LGFunction{
+		"Say":                          robotSay,
+		"SayThread":                    robotSayThread,
+		"ReplyThread":                  robotReplyThread,
+		"Reply":                        robotReply,
+		"SendChannelMessage":           robotSendChannelMessage,
+		"SendChannelThreadMessage":     robotSendChannelThreadMessage,
+		"SendUserMessage":              robotSendUserMessage,
+		"SendUserChannelMessage":       robotSendUserChannelMessage,
+		"SendUserChannelThreadMessage": robotSendUserChannelThreadMessage,
+	}
+
+	// Fetch the existing "robot" metatable. Typically set up in your CallExtension.go.
+	mt := L.NewTypeMetatable("robot")
+	L.SetField(mt, "__index", L.SetFuncs(L.NewTable(), messageMethods))
+}
