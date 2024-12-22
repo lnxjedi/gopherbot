da0419f28209e16e9f7efff36f4acb40c1a60549
David Parsley <parsley@linuxjedi.org>
2024-12-22 22:03:21 +0000

    Add CheckAdmin, Elevate and Log

--- modules/lua/util_methods.go
+++ modules/lua/util_methods.go
@@ -7,32 +7,30 @@ import (
 	glua "github.com/yuin/gopher-lua"
 )
 
-// RegisterUtilMethods adds RandomInt, RandomString, and Pause to the robot's metatable
+// RegisterUtilMethods adds RandomInt, RandomString, Pause, CheckAdmin, Elevate, and Log to the robot's metatable
 func RegisterUtilMethods(L *glua.LState) {
 	methods := map[string]glua.LGFunction{
 		"RandomInt":    robotRandomInt,
 		"RandomString": robotRandomString,
 		"Pause":        robotPause,
+		"CheckAdmin":   robotCheckAdmin,
+		"Elevate":      robotElevate,
+		"Log":          robotLog,
 	}
 	robotIndex := getRobotMethodTable(L)
 	L.SetFuncs(robotIndex, methods)
 }
 
-// robotRandomInt(luaState) -> int
-//
-// Usage in Lua:
-//
-//	local x = robot:RandomInt(10)  -- x in [0..9]
-//
-// If invalid input is provided, logs an error and returns 0.
+// robotRandomInt wraps r.RandomInt
 func robotRandomInt(L *glua.LState) int {
-	// Arg 1: robot userdata, Arg 2: upperBound
 	ud := L.CheckUserData(1)
 	nLua := L.Get(2)
 
 	lr, ok := ud.Value.(*luaRobot)
 	if !ok {
-		lr.r.Log(robot.Error, "RandomInt called with invalid robot userdata")
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "RandomInt called with invalid robot userdata")
+		}
 		L.Push(glua.LNumber(0))
 		return 1
 	}
@@ -45,33 +43,24 @@ func robotRandomInt(L *glua.LState) int {
 
 	n := int(nLua.(glua.LNumber))
 	val := lr.r.RandomInt(n)
-
 	L.Push(glua.LNumber(val))
 	return 1
 }
 
-// robotRandomString(luaState) -> string
-//
-// Usage in Lua:
-//
-//	local arr = { "hello", "there", "people" }
-//	local s = robot:RandomString(arr)
-//	-- s is one of the above
-//
-// If invalid input or array is empty, logs an error and returns "".
+// robotRandomString implements r.RandomString(...)
 func robotRandomString(L *glua.LState) int {
-	// Arg 1: robot userdata, Arg 2: table of strings
 	ud := L.CheckUserData(1)
 	arrLua := L.Get(2)
 
 	lr, ok := ud.Value.(*luaRobot)
 	if !ok {
-		lr.r.Log(robot.Error, "RandomString called with invalid robot userdata")
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "RandomString called with invalid robot userdata")
+		}
 		L.Push(glua.LString(""))
 		return 1
 	}
 
-	// Must be a table
 	if arrLua.Type() != glua.LTTable {
 		lr.r.Log(robot.Error, "RandomString called with non-table argument")
 		L.Push(glua.LString(""))
@@ -79,7 +68,7 @@ func robotRandomString(L *glua.LState) int {
 	}
 
 	tbl := arrLua.(*glua.LTable)
-	length := tbl.Len() // 1-based length
+	length := tbl.Len()
 
 	var goSlice []string
 	for i := 1; i <= length; i++ {
@@ -87,8 +76,7 @@ func robotRandomString(L *glua.LState) int {
 		if val.Type() == glua.LTString {
 			goSlice = append(goSlice, val.String())
 		} else {
-			lr.r.Log(robot.Error, fmt.Sprintf(
-				"RandomString: non-string element at index %d, ignoring", i))
+			lr.r.Log(robot.Error, fmt.Sprintf("RandomString: non-string element at index %d, ignoring", i))
 		}
 	}
 
@@ -103,20 +91,16 @@ func robotRandomString(L *glua.LState) int {
 	return 1
 }
 
-// robotPause(luaState) -> (no return value)
-//
-// Usage in Lua:
-//
-//	robot:Pause(2.5)  -- sleeps 2.5 seconds
-//
-// If invalid input is provided, logs an error and still returns no value.
+// robotPause wraps r.Pause(...)
 func robotPause(L *glua.LState) int {
 	ud := L.CheckUserData(1)
 	secLua := L.Get(2)
 
 	lr, ok := ud.Value.(*luaRobot)
 	if !ok {
-		lr.r.Log(robot.Error, "Pause called with invalid robot userdata")
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "Pause called with invalid robot userdata")
+		}
 		return 0
 	}
 
@@ -126,9 +110,88 @@ func robotPause(L *glua.LState) int {
 	}
 
 	sec := float64(secLua.(glua.LNumber))
-	// Use the robot's Pause method to handle fractional seconds
 	lr.r.Pause(sec)
+	return 0
+}
 
-	// No return value
+// robot:CheckAdmin() -> bool
+func robotCheckAdmin(L *glua.LState) int {
+	ud := L.CheckUserData(1)
+	lr, ok := ud.Value.(*luaRobot)
+	if !ok || lr == nil || lr.r == nil {
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "CheckAdmin called with invalid robot userdata")
+		}
+		L.Push(glua.LBool(false))
+		return 1
+	}
+
+	isAdmin := lr.r.CheckAdmin()
+	L.Push(glua.LBool(isAdmin))
+	return 1
+}
+
+// robot:Elevate(immediate) -> bool
+// immediate is a Lua boolean indicating whether to forcibly prompt for 2fa
+func robotElevate(L *glua.LState) int {
+	ud := L.CheckUserData(1)
+	immArg := L.Get(2)
+
+	lr, ok := ud.Value.(*luaRobot)
+	if !ok || lr == nil || lr.r == nil {
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "Elevate called with invalid robot userdata")
+		}
+		L.Push(glua.LBool(false))
+		return 1
+	}
+
+	immediate := false
+	if immArg.Type() == glua.LTBool {
+		immediate = bool(immArg.(glua.LBool))
+	} else {
+		// If user didn't pass a bool, log and treat it as false
+		lr.r.Log(robot.Error, fmt.Sprintf("Elevate called with non-boolean argument (%s), defaulting to false", immArg.Type().String()))
+	}
+
+	success := lr.r.Elevate(immediate)
+	L.Push(glua.LBool(success))
+	return 1
+}
+
+// robot:Log(level, message) -> no return
+// level: numeric log level (logTrace=0, logDebug=1, logInfo=2, etc.)
+// message: string
+func robotLog(L *glua.LState) int {
+	ud := L.CheckUserData(1)
+	levelArg := L.Get(2)
+	msgArg := L.Get(3)
+
+	lr, ok := ud.Value.(*luaRobot)
+	if !ok || lr == nil || lr.r == nil {
+		if lr != nil && lr.r != nil {
+			lr.r.Log(robot.Error, "Log called with invalid robot userdata")
+		}
+		return 0
+	}
+
+	var lvl robot.LogLevel = robot.Info // default
+	if levelArg.Type() == glua.LTNumber {
+		numLevel := int(levelArg.(glua.LNumber))
+		lvl = robot.LogLevel(numLevel)
+	} else {
+		lr.r.Log(robot.Error, fmt.Sprintf("Log: expected numeric level, got %s; defaulting to Info", levelArg.Type().String()))
+	}
+
+	var msg string
+	if msgArg.Type() == glua.LTString {
+		msg = msgArg.String()
+	} else {
+		msg = fmt.Sprintf("Log called with non-string message type: %s", msgArg.Type().String())
+		lr.r.Log(robot.Error, msg)
+		return 0
+	}
+
+	lr.r.Log(lvl, msg)
 	return 0
 }
