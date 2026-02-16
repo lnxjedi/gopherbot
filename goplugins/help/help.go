892a224811241fc19f2fc576f46908b785a465d9
David Parsley <david@welldhealth.com>
2026-02-16 15:45:05 -0500

    Refresh ambient help text and restore default demo user roster

--- goplugins/help/help.go
+++ goplugins/help/help.go
@@ -17,13 +17,19 @@ func help(bot robot.Robot, command string, args ...string) (retval robot.TaskRet
 		botAlias := bot.GetBotAttribute("alias").String()
 		reply := "Hi, I'm "
 		reply += strings.Title(botName) + ", a staff robot. I see you've asked for help.\n\n"
-		reply += "I've been programmed to perform a variety of tasks for your team, and will respond when you send me commands that match specific patterns. " +
-			"For instance, you can activate my built-in help function by sending a message like this:\n\n"
-		reply += botName + ", help ping\nor:\nhelp ping, " + botName + "\n\n... which would give you help on my ping command.\n\n"
+		reply += "I've been programmed to perform a variety of tasks for your team, and I'll respond when you send me commands that match specific patterns. " +
+			"You can ask for command help by addressing me with my name, for example:\n\n"
+		reply += botName + ", help ping\nor:\nhelp ping, " + botName + "\n\n"
 		if len(botAlias) > 0 {
-			reply += "To save a little typing, you can also address messages to me by prefixing it with my alias ( " + botAlias + " ), like this:\n\n" + botAlias + "help ping\n\n"
+			reply += "To save a little typing, you can also use my alias ( " + botAlias + " ). For example:\n\n" + botAlias + "help ping\n\n"
 		}
-		reply += "When the help text for a command has (something) in parentheses, that term or phrase is optional. If <something> is in angle brackets, it's required. With the help function, if you don't supply a keyword you will get help for every command available to you in the current channel, which may differ between channels depending on each channel's purpose. If you use a keyword, you will get help for every command with a matching keyword, along with the channels where it can be used. If the help text is too long, I'll send you a direct message so the channels don't fill up with help output.\n\n"
+		reply += "Here are the most useful discovery commands:\n\n"
+		reply += "• " + botAlias + "help - quick help and pointers\n"
+		reply += "• " + botAlias + "commands - browse command groups available in this channel\n"
+		reply += "• " + botAlias + "help <keyword> - show the best matches with usage and examples\n"
+		reply += "• " + botAlias + "help-all - detailed help for commands available here\n\n"
+		reply += "If I can't match a command, I'll usually reply with the closest matches and a suggested next help command so you can recover quickly.\n\n"
+		reply += "When command help shows syntax, (something) means optional and <something> means required. Command availability can vary by channel, direct message, and permissions, so help output may differ from place to place.\n\n"
 		reply += "Also, from time to I may ask you a question, prompting for additional information - these messages will mention you by name if not in a private conversation. You only need to type your reply - if you address me by name (or alias), I'll consider it a new command and send an error to the plugin requesting input. Additionally, there are two special replies I understand: \"=\" means 'use the default value', whatever that might be; \"-\" means 'cancel', returning an error value to the plugin.\n\n"
 		if bot.GetMessage().Protocol.String() == "Terminal" {
 			reply += "Since you're using the 'terminal' connector, you can get simple help for changing the user, channel and thread just by hitting <return> with an empty command.\n\n"
