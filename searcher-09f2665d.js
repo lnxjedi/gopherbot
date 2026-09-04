40c6f2eeac6462d2b539f5f56a572758ffbf39b1
David Parsley <parsley@linuxjedi.org>
2026-09-04 12:15:20 +0000

    Automated update from 43f3893a7187af1cc3e08c5e305aa7af5794438f

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f528795f.js',
+                path_to_root + 'searchindex-f5186057.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
